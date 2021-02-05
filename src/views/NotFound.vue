af6c09bead05dce1a4cd5239e888a7decc30c4e1
Kaung-HtetKyaw <kaunghtetkyaw02749@gmail.com>
2021-02-05 22:26:36 +0630

    fix: not showing the product because of the absence of the industry

---
+++ src/views/NotFound.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="12" sm="12">
+        <v-img src="/logo.PNG"></v-img>
+        <h1>Not Found</h1>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
